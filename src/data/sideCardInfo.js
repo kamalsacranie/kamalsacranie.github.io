e1dc5773f7b463646b3da259f089ade432dc9548
Kamal Sacranie <kamalsjunkmail@gmail.com>
2021-08-13 03:23:33 +0100

    Finish fist bit of About page

---
+++ src/data/sideCardInfo.js
@@ -0,0 +1,22 @@
+const cardInfo = {
+    funFacts: [
+        {
+            fact: 'Favourite 🍹',
+            answer: 'Long Island Ice Tea',
+        },
+        {
+            fact: '⚽️ team',
+            answer: 'Manchester United',
+        },
+        {
+            fact: 'Home country',
+            answer: 'South Africa 🇿🇦',
+        },
+        {
+            fact: 'Current home',
+            answer: 'England 🏴󠁧󠁢󠁥󠁮󠁧󠁿',
+        },
+    ],
+}
+
+export default cardInfo
